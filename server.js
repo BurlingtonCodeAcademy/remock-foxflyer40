3018ad8c84630da2dcafa533d5b8ee4b9b67835a
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-11-04 20:12:19 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
+// bring in express
 const express = require('express')
+const app = express() 
+// set up port options
+const port = process.env.PORT || 5000  
+// create absolute paths
 const path = require('path')  
 
-const app = express() 
-const port = process.env.PORT || 5000  
+// middleware
+//static routes
+app.use(express.static('./public'))
 
-
-app.use(express.static(path.resolve('./client/build'))) 
-
-
-app.get('*', (request, response) =>{
-response.sendFile(path.resolve('./client/build/index.html'))
+// route to home page *****************************
+app.get('/', (request, response) =>{
+response.sendFile(path.resolve('./index.html'))
 })  
 
+// blog-post routes  *******************************
+app.get('/gym-wear', (request, response) => {
+   response.sendFile(path.resolve('./public/blog-post-gym-wear.html'))
+})  
 
+app.get('/party', (request, response) => {
+   response.sendFile(path.resolve('./public/blog-post-party.html'))
+})  
+
+app.get('/slacks', (request, response) => {
+   response.sendFile(path.resolve('./public/blog-post-slacks.html'))
+})  
+
+// continue reading routes  ***********************
+app.get('/crochet', (request, response) => {
+   response.sendFile(path.resolve('./public/continue-reading-crochet.html'))
+})  
+
+app.get('/sweaters', (request, response) => {
+   response.sendFile(path.resolve('./public/continue-reading-sweater-belt.html'))
+})  
+
+ // learn more routes  ****************************
+app.get('/moobs', (request, response) => {
+   response.sendFile(path.resolve('./public/learn-more-moobs.html'))
+})  
+
+app.get('/plaid', (request, response) => {
+   response.sendFile(path.resolve('./public/learn-more-plaid.html'))
+})  
+
+// start the server
 app.listen(port, () => { console.log(`Listening on port: ${port}`)})  
